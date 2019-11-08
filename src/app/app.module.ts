e255cbb94f5ea4b3a89c4a228aa211dacace1860
Ambar Mutha <ambar.ytl@gmail.com>
2019-11-08 17:51:58 +0530

    generate components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './pages/home/results/results.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, TopBarComponent, HomeComponent, SearchComponent, ResultsComponent],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
