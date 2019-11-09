740d4c7d8f62913e59cc84ba7005bbad2e1fe68a
Ambar Mutha <ambar.ytl@gmail.com>
2019-11-09 11:34:53 +0530

    get search string from search component disable search when textbox is empty

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+	searchString;
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 
+	onSearch($event) {
+		this.searchString = $event;
+		console.log(this.searchString);
+	}
 }
--- src/app/pages/home/search/search.component.ts
+++ src/app/pages/home/search/search.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+	selector: 'app-search',
+	templateUrl: './search.component.html',
+	styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+	searchString = '';
+	constructor() {}
 
-  constructor() { }
+	@Output() searchEvent = new EventEmitter<string>();
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 
+	triggerSearch() {
+		this.searchEvent.emit(this.searchString);
+	}
 }
