c234c8a313777ed1d3db46e17a4046b48d2751fa
Ambar Mutha <ambar.ytl@gmail.com>
2019-11-09 17:27:51 +0530

    display cards in results update build task

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,10 +5,28 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
 
 @NgModule({
 	declarations: [],
-	imports: [MatToolbarModule, MatInputModule, MatFormFieldModule, MatIconModule, MatButtonModule],
-	exports: [MatToolbarModule, MatInputModule, MatFormFieldModule, MatIconModule, MatButtonModule]
+	imports: [
+		MatToolbarModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatButtonModule,
+		MatCardModule,
+		MatRippleModule
+	],
+	exports: [
+		MatToolbarModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatButtonModule,
+		MatCardModule,
+		MatRippleModule
+	]
 })
 export class MaterialModule {}
