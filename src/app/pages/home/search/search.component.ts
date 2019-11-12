594591a372dbd7fd653760f3253c83a2245747a2
Ambar Mutha <ambar.ytl@gmail.com>
2019-11-12 19:43:58 +0530

    add voice search feature

--- src/app/pages/home/search/search.component.ts
+++ src/app/pages/home/search/search.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
+import { MatInput } from '@angular/material/input';
+
+interface IWindow extends Window {
+	webkitSpeechRecognition: any;
+}
+
+const { webkitSpeechRecognition }: IWindow = window as IWindow;
 
 @Component({
 	selector: 'app-search',
@@ -6,8 +13,37 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 	styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-	searchString = '';
-	constructor() {}
+	@ViewChild('searchInput', { static: true }) searchInput: MatInput;
+	// tslint:disable-next-line: variable-name
+	private _searchString = '';
+	public get searchString() {
+		return this._searchString;
+	}
+	public set searchString(value) {
+		this._searchString = value;
+		this.searchInput.focus();
+		console.log('focused');
+	}
+
+	speechRecognition: SpeechRecognition;
+
+	constructor() {
+		this.speechRecognition = new webkitSpeechRecognition();
+		this.speechRecognition.interimResults = true;
+
+		this.speechRecognition.onstart = _ => console.log('started listening');
+		this.speechRecognition.onend = _ => console.log('stopped listening');
+
+		this.speechRecognition.onresult = event => {
+			for (let i = event.resultIndex; i < event.results.length; ++i) {
+				if (event.results[i].isFinal) {
+					this.searchString += event.results[i][0].transcript;
+				}
+			}
+		};
+
+		this.speechRecognition.lang = 'en-US';
+	}
 
 	@Output() searchEvent = new EventEmitter<string>();
 
