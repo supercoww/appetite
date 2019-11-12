cbd5c7dfdf51e770f6e352ca9b72730a0107354c
Saransh <markkavens06@gmail.com>
2019-11-12 19:28:57 +0530

    image to text feature done

--- src/app/pages/home/search/search.component.ts
+++ src/app/pages/home/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { createWorker } from 'tesseract.js';
 
 @Component({
 	selector: 'app-search',
@@ -7,6 +8,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 	searchString = '';
+
 	constructor() {}
 
 	@Output() searchEvent = new EventEmitter<string>();
@@ -16,4 +18,38 @@ export class SearchComponent implements OnInit {
 	triggerSearch() {
 		this.searchEvent.emit(this.searchString);
 	}
+
+	clickRealImgBtn(){
+		document.getElementById("realImageUpload").click();	
+	}
+
+	imageUpload(){
+		console.log("imageupload");
+		var fileUp = document.getElementById("realImageUpload");
+		console.log(fileUp.files);
+		var fileOb = fileUp.files;
+
+		const worker = createWorker({
+			logger: m => console.log(m), // Add logger here
+		  });
+		  
+		  (async () => {
+				await worker.load();
+				await worker.loadLanguage('eng+chi_tra');
+				await worker.initialize('eng+chi_tra');
+				
+				const { data: { text } } = await worker.recognize(fileOb[0]);
+
+				// TODO :- SPINNER 
+				var imgText=this.formatQuery(text)
+				console.log(imgText);
+				this.searchString=imgText;
+				await worker.terminate();
+		  })();
+		}
+
+		formatQuery(text)
+		{
+			return text.replace(/(\r\n|\n|\r)/gm," "); // replacing newlines with space
+		}
 }
