d31dfe6c8e811a6a61b56062b1c7d054f4dbd6f0
Ambar Mutha <ambar.ytl@gmail.com>
2019-11-13 19:17:17 +0530

    update filter ui and move it to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'Stack Search';
+	showFilters = false;
+	topBarFilterIcon = 'filter_list';
+
+	toggleFilterMenu() {
+		this.showFilters = !this.showFilters;
+		this.topBarFilterIcon = this.topBarFilterIcon === 'close' ? 'filter_list' : 'close';
+	}
 }
