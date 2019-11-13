0dab158e5bfe02cb07c867ef6896ea77bdfa2a14
Ambar Mutha <ambar.ytl@gmail.com>
2019-11-13 19:43:04 +0530

    show actual search results

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -19,26 +19,8 @@ export class HomeComponent implements OnInit {
 		this.searchResults = this.getSearchResults();
 	}
 
-	// TODO: replace demo results with actual
+	// TODO: pass filter and sort data
 	getSearchResults() {
-		this.dataService.fetchresults(this.searchString);
-		return [
-			{
-				score: 10,
-				answer_count: 5,
-				view_count: 100,
-				link:
-					'https://stackoverflow.com/questions/56400290/white-screen-on-fresh-new-angular-8-electron-5-app',
-				creation_date: 1573560621098,
-				last_activity_date: 1573560621098,
-				title: 'We want fridge',
-				is_answered: true,
-				owner: {
-					display_name: 'spongebob',
-					profile_image:
-						'https://www.gravatar.com/avatar/6c8986ef36d3e6a1edcdaf6ecea15a6d?s=32&d=identicon&r=PG&f=1'
-				}
-			}
-		];
+		return this.dataService.fetchresults({ text: this.searchString });
 	}
 }
