7c7b9f24a8bbf046b92257adb2139b13ee5e0b4c
Ambar Mutha <ambar.ytl@gmail.com>
2019-11-16 20:26:33 +0530

    remove unused packages minor design changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,7 @@ import { ResultsComponent } from './pages/home/results/results.component';
 import { SearchComponent } from './pages/home/search/search.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FiltersComponent } from './pages/home/filters/filters.component';
-import { ImageCropperModule } from 'ngx-image-cropper';
 
-import { AngularCropperjsModule } from 'angular-cropperjs';
 import { HistoryComponent } from './pages/history/history.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
@@ -36,9 +34,7 @@ import { environment } from '../environments/environment';
 		BrowserAnimationsModule,
 		MaterialModule,
 		FormsModule,
-		ImageCropperModule,
 		HttpClientModule,
-		AngularCropperjsModule,
 		ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
 	],
 
--- src/app/pages/home/search/search.component.ts
+++ src/app/pages/home/search/search.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit, Output, EventEmitter, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { MatInput } from '@angular/material/input';
-import * as Tesseract from 'tesseract.js';
-import { ImageCroppedEvent, ImageCropperComponent } from 'ngx-image-cropper';
 import 'firebase/storage';
 import 'firebase/app';
-import { CropperComponent } from 'angular-cropperjs';
 import { HistoryService } from 'src/app/history.service';
-import imageCompression from 'browser-image-compression'
+import imageCompression from 'browser-image-compression';
+
 interface IWindow extends Window {
 	webkitSpeechRecognition: any;
 }
@@ -19,8 +17,8 @@ const { webkitSpeechRecognition }: IWindow = (window as any) as IWindow;
 	styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-	compress=true;
-	minsizetocompress=1000000;
+	compress = true;
+	minsizetocompress = 1000000;
 	storage = null;
 	firebase = require('firebase/app');
 	apiurl = 'https://protected-mesa-37941.herokuapp.com/?url=';
@@ -164,10 +162,10 @@ export class SearchComponent implements OnInit {
 			alert('file too large');
 			return;
 		}
-		var need=true;
-		if(fileOb.size<this.minsizetocompress)need=false;
+		var need = true;
+		if (fileOb.size < this.minsizetocompress) need = false;
 		console.log('processing');
-		this.compressimg(fileOb,need);
+		this.compressimg(fileOb, need);
 
 		/*	var reader = new FileReader();
 		reader.readAsDataURL(fileOb);
@@ -227,32 +225,26 @@ export class SearchComponent implements OnInit {
 			});
 	}
 
-	compressimg(img,need)
-	{
-		if(this.compress==false || need==false)
-		{
+	compressimg(img, need) {
+		if (this.compress == false || need == false) {
 			this.processimg(img);
 			return;
 		}
-		
-		var imageFile =img
+
+		var imageFile = img;
 		console.log('originalFile instanceof Blob', imageFile instanceof Blob); // true
 		console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
-	   
+
 		var options = {
-		  maxSizeMB: 1,
-		  maxWidthOrHeight: 1920,
-		  useWebWorker: true
-		}
-		var self=this;
-		imageCompression(imageFile, options)
-		  .then(function (compressedFile) {
+			maxSizeMB: 1,
+			maxWidthOrHeight: 1920,
+			useWebWorker: true
+		};
+		var self = this;
+		imageCompression(imageFile, options).then(function(compressedFile) {
 			console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
 			console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
 			self.processimg(compressedFile);
-			
-		  });
-		  
+		});
 	}
 }
-
